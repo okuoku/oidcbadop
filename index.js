3635fb5360d027a13ba8d27be5787e9dbbbc06bb
okuoku <mjt@cltn.org>
2019-09-10 05:58:15 +0900

    Add valid-token feature

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Provider = require("oidc-provider");
 const Koa = require("koa");
 const KoaMount = require("koa-mount");
 const KoaRouter = require("koa-router");
+const KoaEjs = require("koa-ejs");
 const Jose = require("@panva/jose");
 const crypto = require("crypto");
-const CookieKeys = new Array(crypto.randomBytes(32));
+const path = require("path");
+const DummyCookieKeys = new Array(crypto.randomBytes(32).toString("base64"));
 
 const issroot = "http://localhost:3000/op";
 
@@ -16,8 +18,8 @@ const oidc_config = {
     jwks: keystore_jwks,
     findAccount: (ctx, id) => {
         return {
-            accountId: id,
-            claims: (use, scope) => Promise.resolve({sub: id})
+            accountId: "dummy",
+            claims: (use, scope) => Promise.resolve({sub: "dummy", modify: id})
         };
     },
     features: {
@@ -30,26 +32,55 @@ const oidc_config = {
     clients: [{
         client_id: "testing",
         client_secret: "testing",
-        redirect_uris: ["https://example.com:80"]
+        redirect_uris: ["https://example.com:80"],
+        default_max_age: 1 /* Always request login */
     }],
     interactions: {
-        url: (ctx, interaction) => Promise.resolve("/passthrough")
+        url: (ctx, interaction) => Promise.resolve("/gate")
     },
     cookies: {
-        keys: CookieKeys,
-        short: {signed: true},
-        long: {signed: true}
+        keys: DummyCookieKeys, // FIXME: Sign cookies
+        short: {signed: false},
+        long: {signed: false}
+    },
+    // Custom id_token claim(s)
+    claims: {
+        // Standard claims
+        acr: null,
+        iss: null,
+        auth_time: null,
+        sid: null,
+        // Custom: "modify" claim to modify output JWT
+        openid: [
+            "sub", "modify",
+        ]
     }
+};
+
+const ejs_config = {
+    root: path.join(__dirname, "view"),
+    layout: "_base",
+    viewExt: "html",
+    cache: true,
+    debug: true
 };
 
 const oidc = new Provider(issroot, oidc_config);
 
-async function passthrough(ctx, next){
+async function gate(ctx, next){
+    return ctx.render("chooser");
+}
+
+async function chooser(ctx, next){
     const details = await oidc.interactionDetails(ctx.req, ctx.res);
+    let choice = "dummy";
+    if(ctx.params.choice){
+        choice = ctx.params.choice;
+    }
     const result = {
         login: {
-            account: "dummy",
-            acr: "dummy",
+            account: choice,
+            acr: "0",
             remember: false,
         },
         consent: {
@@ -57,8 +88,11 @@ async function passthrough(ctx, next){
             rejectedClaims: []
         }
     };
+
     return oidc.interactionFinished(ctx.req, ctx.res, result);
 }
+
+// FIXME: Implement catchall
 
 async function result_filter(ctx, next){
     await next();
@@ -66,7 +100,14 @@ async function result_filter(ctx, next){
         let token = ctx.response.body;
         let valid_token = token.id_token;
         let q = Jose.JWT.decode(valid_token);
-        q.exp = q.iat - 1;
+        switch(q.modify){
+            case "expire":
+                q.exp = q.iat - 1;
+                break;
+            default:
+                /* Do nothing */
+                break;
+        }
         let x = Jose.JWT.sign(q, keystore.get({alg: "RS256"}));
         token.id_token = x;
     }
@@ -78,12 +119,14 @@ const router = new KoaRouter();
 // Disable client redirect uri check
 oidc.Client.prototype.redirectUriAllowed = (bogus) => true;
 
-router.get("/passthrough", passthrough);
+router.get("/gate", gate);
+router.get("/gate/chooser/:choice", chooser); // Need to be under "/gate"
 app.use(router.routes())
    .use(router.allowedMethods());
-app.use(KoaMount("/op", oidc.app));
 
 oidc.use(result_filter);
+KoaEjs(app, ejs_config);
+app.use(KoaMount("/op", oidc.app));
 
 app.proxy = true;
 
